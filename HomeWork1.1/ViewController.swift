07874ae8304b0ec1c49a8bc89a5f94e4731f5a84
Gleb Trubanov <crew.gleb@gmail.com>
2021-12-28 20:25:11 +0300

    add two rect's

--- HomeWork1.1/ViewController.swift
+++ HomeWork1.1/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var rectUP: UILabel!
+    
+    
+    @IBOutlet var rectDown: UILabel!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
